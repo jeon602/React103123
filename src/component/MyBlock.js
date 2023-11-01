5431a7cba423a12950755aaf7d1f74d6fe7f8aa9
jeon602 <731aa@naver.com>
2023-11-01 16:42:01 +0900

    css module을 사용할 때 여러 클래스면을 주는 방법 연습 시각-> template literal 또는  js array.join으로 해결

--- src/component/MyBlock.js
+++ src/component/MyBlock.js
@@ -3,6 +3,8 @@ export function MyBlock() {
   return (
     <div>
       <h1 className={block.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={block.special}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={block.coffee}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -6,6 +6,8 @@ export function MyText() {
       {/* src/style/mystyle.module.css 파일 생성 */}
       {/* MyText.js 에서 mystyle.module.css import 하기 */}
       <h1 className={my.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={my.special}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={my.coffee}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
