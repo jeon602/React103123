8d72bff2e7a69dcc79022d5f81b60d7b1f7b753d
jeon602 <731aa@naver.com>
2023-11-01 12:25:57 +0900

    props 작성, 활용연습 완료

---
+++ src/MyBox.js
@@ -0,0 +1,13 @@
+export function MyBox() {
+  return null;
+}
+export function MyContainer() {
+  return null;
+}
+export function MyElem() {
+  return null;
+}
+export let name = "손흥민";
+export const address = "신촌";
+
+export const person = { name: "강인", team: "파리" };
