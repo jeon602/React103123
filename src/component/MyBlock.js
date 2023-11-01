16269b10dd633a699f5d2f15a54e90bd09d6a0a3
jeon602 <731aa@naver.com>
2023-11-01 16:21:21 +0900

    css module 연습하기 myblock과 mytext에 다른 css 스타일 적용시키기

---
+++ src/component/MyBlock.js
@@ -0,0 +1,8 @@
+import block from "../style/yourstyle.module.css";
+export function MyBlock() {
+  return (
+    <div>
+      <h1 className={block.error}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,11 +1,11 @@
-import "../style/mystyle.css";
+import my from "../style/mystyle.module.css";
 export function MyText() {
   return (
     <div>
       {/* red 배경색, white 글자색  */}
-      {/* src/style/mystyle.css 파일 생성 */}
-      {/* MyText.js 에서 mystyle.css import 하기 */}
-      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+      {/* src/style/mystyle.module.css 파일 생성 */}
+      {/* MyText.js 에서 mystyle.module.css import 하기 */}
+      <h1 className={my.error}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
