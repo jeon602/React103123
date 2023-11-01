a1102fcf0205675b66a975b64643298f935af88b
jeon602 <731aa@naver.com>
2023-11-01 11:22:44 +0900

    props 구분할 연습. settings : jsx attribute

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 function App() {
   return (
     <>
-      <MyComp value="손흥민" address="신촌" />
+      <MyComp name="손흥민" address="신촌" age={33} city="서울" />
+      <MyComp address="강남" age={23} city="부산" />
+      <MyComp address="런던" age={23} city="영국" />
+      <MyComp name="희찬" address="런던" age={66} city="프랑스" />
     </>
   );
 }
-function MyComp({ value, address }) {
+function MyComp({ name = "anonymous", address, city, age = 100 }) {
   return (
     <div>
-      <h1>
-        {value}은{address}에 산다
-      </h1>
+      <p>
+        {name}은{age}세 이고 {address}, {city}에 거주한다.
+      </p>
     </div>
   );
 }
