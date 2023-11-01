e3a2577208677ad193181398a26d7ec3a528658d
jeon602 <731aa@naver.com>
2023-11-01 10:40:57 +0900

    props 연습 (이름,나이,소속)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 function App() {
-  //jax에서 태그 사용시 꼭 종료태그를 작성해야 함.
-  //빈요소이면 시작 태그에서 종료
   return (
-    <div>
-      <h2>hello react</h2>
-      <h3>HI REACT</h3>
-      <p>
-        Lorem ipsum dorlor .<br />
-        lerem{" "}
-      </p>
-    </div>
+    <>
+      <MyElem name="흥민" age={44} team="토트넘" />
+      <MyElem name="강인" age={23} team="파리" />
+    </>
   );
 }
-//built -in component는 소문자로 시작
-//COMPONENT 명은 반드시 대문자로 시작
-//직접 작성하는 component는 반드시 return값을 가져야 하며
-//보통 return값은 JSX 코드이다 ()
-function MyComp() {
-  //return의 jsx코드 작성 가능
-  //꼭 하나의 root tag(상위 태그)가 존재해야 한다. 두 개의 태그를 사용하면 안되며
-  //div 태그로 감싸서 2개를 작성한다.
-  //return <div><h2>hi component</h2></div>
-  //return값이 여러줄이면 () 로 감싸기
-  //return(
-  // <div>
-  // <h1></h1>
-  // <h2></h2>
-  // </div>)
-  // root tag가 필요없다면 fragment로 사용가능하다 <div> 대신 빈 <>
-
-  //jsx에서 주석사용법
-  // {/* */}
-  // {/* fragment는 <></>*/}
-  return <h2>hi component</h2>;
+function MyElem({ name, age, team }) {
+  return (
+    <div>
+      <h2>
+        {name}의 나이는{age}, {team}소속의 선수
+      </h2>
+    </div>
+  );
 }
 export default App;
