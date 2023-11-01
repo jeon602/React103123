e6b6b46d184cea9d9bab21e26f57d3c66bdedbb9
jeon602 <731aa@naver.com>
2023-11-01 14:51:43 +0900

    여러 파일에서 중복된 named export를 import하는 방법 연습 14:51 1101

---
+++ src/MyComp1101/Box.js
@@ -0,0 +1,8 @@
+export default function Box({ style }) {
+  return (
+    <div style={style}>
+      <h1>THIS IS BOX</h1>
+    </div>
+  );
+}
+export const color = "purple";
---
+++ src/MyComp1101/Container.js
@@ -0,0 +1,8 @@
+export default function Container({ style }) {
+  return (
+    <div style={style}>
+      <h2>HI. CONAINER</h2>
+    </div>
+  );
+}
+export const color = "red";
