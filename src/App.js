c124baa780de511d684b0ca35c723e17056e175a
jeon602 <731aa@naver.com>
2023-11-02 10:23:36 +0900

    필요없는 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-import { Box } from "@chakra-ui/react";
-
 function App() {
-  return (
-    <>
-      <Box bgColor="tomato"> SORAN-AAA </Box>
-      <Box bgColor="yellow"> DAYBRAKE </Box>
-      <Box bgColor="blue">SURL-SNOWMAN</Box>
-      <Box bgColor="blue.100">dsfkvhsl</Box>
-      <Box bgColor="beige.800">Cults -FOREVER</Box>
-      <Box bgColor="remon.200">Cults -FOREVER</Box>
-      <Box bgColor="orange.1">Cults -FOREVER</Box>
-      <Box bgColor="skyblue.800">Cults -FOREVER</Box>
-      <Box bgColor="gray.800">Cults -FOREVER</Box>
-    </>
-  );
+  return <></>;
 }
 export default App;
