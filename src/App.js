2e70d930e3d81daafb05c2cc9f21232d04be15fe
jeon602 <731aa@naver.com>
2023-11-02 11:19:49 +0900

    flex box 사용

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import { Box } from "@chakra-ui/react";
+import { Box, flex } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Box bg={"green.100"} border={"3px dotted orange"}>
-        Cults__ Forever🎵
-      </Box>
-      <Box bg={"pink.50"} border={"2px solid blue"}>
-        Minsu __ XXLOVE🎵
-      </Box>
-      <Box borderTop={"2px double red"}>Inventore, minima.</Box>
-      <Box borderY={"10px solid"}>Dignissimos, temporibus?</Box>
-
-      <Box border={"5px solid darkgreen"} borderRadius={"50px"}>
-        {" "}
-        Blanditiis, error.
-      </Box>
-      <Box border={"5px solid darkgreen"} borderRadius={"15%"}>
-        {" "}
-        Deserunt, fugit.
-      </Box>
-      <Box color={"orange"}>찬주 __ zZz🎵</Box>
-      <Box border={"5px solid brown"} borderTopLeftRadius={10}>
-        SURL __ 열기구🎵
-      </Box>
-      <Box shadow={"10px 5px 2px"}>Laudantium, nam.</Box>
-      <Box bg={"lightpink"}>MAE MULLER _ JENNEY🎵</Box>
-      <Box bg="red" m="2" color="white">
-        알레프 _ 놀이동산
-      </Box>
-      <Box bg="red.100">Lorem ipsum.</Box>
-      <Box bg="orange.50">Nostrum, quod.</Box>
-      <Box bg="lightgray" color="white">
-        Asperiores, praesentium?
-      </Box>
-      <Box bg="green" pl={"20px"}>
-        Ad, quod.
-      </Box>
-      <Box bg="pink" pl={"20px"}>
-        Cupiditate, voluptatibus!
-      </Box>
-      <Box bg="blue.10">Ex, quod?</Box>
-      <Box bg="">Quo, voluptatibus?</Box>
-      <Box bg="">Nisi, obcaecati.</Box>
-      <Box bg="">Sunt, veritatis?</Box>
-      <Box bg="">Ex, optio.</Box>
-
-      <Box>Molestias, voluptatum?</Box>
-      <Box>Non, veritatis!</Box>
-      <Box>Repellendus, sed!</Box>
+      <flex>
+        <Box
+          bg={"gold"}
+          w={"100%"}
+          h={"100px"}
+          gap={3}
+          align={"center"}
+          fontSize={"4rem"}
+          justify={"space-around"}
+        >
+          Lorem ipsum.
+        </Box>
+        <Box bg={"orange"}>Eligendi, laudantium.</Box>
+        <Box bg={"blue"}>Nihil, suscipit.</Box>
+        <Box bg={"pink"}>Quae, quas!</Box>
+        <Box bg={"green"}>Iusto, tenetur.</Box>
+      </flex>
     </>
   );
 }
