efa4766049f90759b36bef7ed57f52161c012c5c
jeon602 <731aa@naver.com>
2023-11-02 14:37:47 +0900

    botton 사용

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import { Button, useToast } from "@chakra-ui/react";
+import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
-
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물이 저장되었습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="skyblue">Lorem.</Button>
+      <Button colorScheme="blue">Iste.</Button>
+      <Button colorScheme="yellowgreen">Est?</Button>
+      <Button colorScheme="tomato">Vero.</Button>
+      <Button colorScheme="skyblue">Voluptates.</Button>
+      <Button colorScheme="orange">Et!</Button>
+      <Button colorScheme="pink">A!</Button>
+      <Button colorScheme="tomato">Harum.</Button>
+      <Button colorScheme="gray">Perspiciatis.</Button>
+      <Button colorScheme="black">Nam.</Button>
+      <Button colorScheme="skyblue">Expedita.</Button>
+      <Button colorScheme="skyblue" variant="outline">
+        Vitae.
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 실패",
-            description: "게시물이 저장되지 않았습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Lorem.</Button>
+      <Button colorScheme="yellow">Numquam!</Button>
+      <Button colorScheme="orange">Eligendi.</Button>
+      <Button colorScheme="teal" size={"sm"}>
+        Dolorum.
+      </Button>
+      <Button colorScheme="messenger" size={"lg"}>
+        Nesciunt.
+      </Button>
+      <Button colorScheme="telegram" variant="outline">
+        Impedit?
+      </Button>
+      <Button colorScheme="orange" variant="ghost">
+        Magni.
+      </Button>
+      <Button variant="link">Quos.</Button>
+      <Button leftIcon={<EmailIcon />}>Quos!</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Optio.</Button>
+      <Button isLoading>Temporibus!</Button>
+      <Button isLoading loadingText="전송중">
+        Quae.
       </Button>
     </>
   );
