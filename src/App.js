8d316678bb0965ffcc29d7ab268593caeb4adc06
jeon602 <731aa@naver.com>
2023-11-02 11:34:32 +0900

    grid or simple grid/ 반응형 웹 적용 simplegrid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import { Box, flex } from "@chakra-ui/react";
+import { Box, flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <flex>
-        <Box
-          bg={"gold"}
-          w={"100%"}
-          h={"100px"}
-          gap={3}
-          align={"center"}
-          fontSize={"4rem"}
-          justify={"space-around"}
-        >
-          Lorem ipsum.
+      <SimpleGrid gap={3} columns={{ base: 4, md: 5, "2xl": 10 }}>
+        <Box bg="lightseagreen" color={"white"}>
+          Lorem.
         </Box>
-        <Box bg={"orange"}>Eligendi, laudantium.</Box>
-        <Box bg={"blue"}>Nihil, suscipit.</Box>
-        <Box bg={"pink"}>Quae, quas!</Box>
-        <Box bg={"green"}>Iusto, tenetur.</Box>
-      </flex>
+        <Box bg="skyblue" color={"white"}>
+          Eius.
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Fugiat.
+        </Box>
+        <Box bg="green" color={"white"}>
+          Ratione!
+        </Box>
+        <Box bg="gray" color={"white"}>
+          Officiis.
+        </Box>
+        <Box bg="paleturquoise" color={"white"}>
+          Non?
+        </Box>
+        <Box bg="palevioletred" color={"white"}>
+          Odio?
+        </Box>
+        <Box bg="blanchedalmond" color={"white"}>
+          Eligendi?
+        </Box>
+        <Box bg="yellowgreen" color={"white"}>
+          Officia.
+        </Box>
+        <Box bg="tomato" color={"white"}>
+          Iusto!
+        </Box>
+        <Box bg="gray" color={"white"}>
+          Nulla.
+        </Box>
+        <Box bg="purple" color={"white"}>
+          A!
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Rem?
+        </Box>
+        <Box bg="yellowgreen" color={"white"}>
+          Nemo.
+        </Box>
+        <Box bg="pink" color={"white"}>
+          Iusto.
+        </Box>
+        <Box bg="blackAlpha.50" color={"white"}>
+          Veritatis.
+        </Box>
+        <Box bg="paleturquoise" color={"white"}>
+          Eum.
+        </Box>
+        <Box bg="yellow" color={"white"}>
+          Vitae.
+        </Box>
+        <Box bg="paleturquoise" color={"white"}>
+          Mollitia!
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Animi.
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Expedita.
+        </Box>
+        <Box bg="paleturquoise" color={"white"}>
+          Illum.
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Adipisci!
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Tempore?
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Excepturi.
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Quaerat.
+        </Box>
+        <Box bg="magenta" color={"white"}>
+          Quibusdam.
+        </Box>
+        <Box bg="paleturquoise" color={"white"}>
+          Libero?
+        </Box>
+        <Box bg="blue" color={"white"}>
+          Dolore!
+        </Box>
+        <Box bg="paleturquoise" color={"white"}>
+          Quidem!
+        </Box>
+      </SimpleGrid>
     </>
   );
 }
