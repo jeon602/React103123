dd0119906dd07ac0d07722cccabcd88b3e358380
jeon602 <731aa@naver.com>
2023-11-02 11:11:59 +0900

    box border

--- src/App.js
+++ src/App.js
@@ -1,4 +1,56 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box bg={"green.100"} border={"3px dotted orange"}>
+        Cults__ Forever🎵
+      </Box>
+      <Box bg={"pink.50"} border={"2px solid blue"}>
+        Minsu __ XXLOVE🎵
+      </Box>
+      <Box borderTop={"2px double red"}>Inventore, minima.</Box>
+      <Box borderY={"10px solid"}>Dignissimos, temporibus?</Box>
+
+      <Box border={"5px solid darkgreen"} borderRadius={"50px"}>
+        {" "}
+        Blanditiis, error.
+      </Box>
+      <Box border={"5px solid darkgreen"} borderRadius={"15%"}>
+        {" "}
+        Deserunt, fugit.
+      </Box>
+      <Box color={"orange"}>찬주 __ zZz🎵</Box>
+      <Box border={"5px solid brown"} borderTopLeftRadius={10}>
+        SURL __ 열기구🎵
+      </Box>
+      <Box shadow={"10px 5px 2px"}>Laudantium, nam.</Box>
+      <Box bg={"lightpink"}>MAE MULLER _ JENNEY🎵</Box>
+      <Box bg="red" m="2" color="white">
+        알레프 _ 놀이동산
+      </Box>
+      <Box bg="red.100">Lorem ipsum.</Box>
+      <Box bg="orange.50">Nostrum, quod.</Box>
+      <Box bg="lightgray" color="white">
+        Asperiores, praesentium?
+      </Box>
+      <Box bg="green" pl={"20px"}>
+        Ad, quod.
+      </Box>
+      <Box bg="pink" pl={"20px"}>
+        Cupiditate, voluptatibus!
+      </Box>
+      <Box bg="blue.10">Ex, quod?</Box>
+      <Box bg="">Quo, voluptatibus?</Box>
+      <Box bg="">Nisi, obcaecati.</Box>
+      <Box bg="">Sunt, veritatis?</Box>
+      <Box bg="">Ex, optio.</Box>
+
+      <Box>Molestias, voluptatum?</Box>
+      <Box>Non, veritatis!</Box>
+      <Box>Repellendus, sed!</Box>
+    </>
+  );
 }
+
 export default App;
