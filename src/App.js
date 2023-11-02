743454b0a48ad396eb99c07e5e267fca07a9613a
jeon602 <731aa@naver.com>
2023-11-02 09:34:36 +0900

    필요없는 파일 삭제함

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-//내가 만든 컴포넌트에 스타일 적용시키기
-//style 적용방법 : css 파일 / css module 사용
-
-import { MyBox } from "./MyComp1101m/MyBox";
-import { MyComp } from "./MyComp1101m/MyComp";
-import { MyContainer } from "./MyComp1101m/MyContainer";
-
 //chakraui css library
 function App() {
-  return (
-    <>
-      <MyComp />
-      <MyBox />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/MyComp1101m/MyComp.js
+++
@@ -1,8 +0,0 @@
-import "../style/style1.module.css";
-export function MyComp() {
-  return (
-    <div>
-      <h1 className="boxClass">lorem ipsum dolor sit amet.</h1>
-    </div>
-  );
-}
