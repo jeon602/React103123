7ca15d776551bc0947997029d62d93ba4a77bf9a
jeon602 <731aa@naver.com>
2023-11-02 10:21:20 +0900

    box chakra

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box bgColor="tomato"> SORAN-AAA </Box>
+      <Box bgColor="yellow"> DAYBRAKE </Box>
+      <Box bgColor="blue">SURL-SNOWMAN</Box>
+      <Box bgColor="blue.100">dsfkvhsl</Box>
+      <Box bgColor="beige.800">Cults -FOREVER</Box>
+      <Box bgColor="remon.200">Cults -FOREVER</Box>
+      <Box bgColor="orange.1">Cults -FOREVER</Box>
+      <Box bgColor="skyblue.800">Cults -FOREVER</Box>
+      <Box bgColor="gray.800">Cults -FOREVER</Box>
+    </>
+  );
 }
 export default App;
