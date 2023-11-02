a382d930569e517445e3d779af56e27e3a03127d
jeon602 <731aa@naver.com>
2023-11-02 14:16:18 +0900

    About Modal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,89 @@
-import { Box, Center, Circle, Square } from "@chakra-ui/react";
-import { ChatIcon, PhoneIcon } from "@chakra-ui/icons";
+// import {
+//   Box,
+//   Button,
+//   Center,
+//   Circle,
+//   Modal,
+//   ModalCloseButton,
+//   ModalContent,
+//   ModalHeader,
+//   ModalOverlay,
+//   Square,
+// } from "@chakra-ui/react";
+//
+// function App() {
+//   return (
+//     <>
+//       <Button onClick={onOpen}>open modal</Button>
+//
+//       <Modal
+//         closeOnOverlayClick={false}
+//         isOpen={isOpen}
+//         onClose={onclose}
+//         isCentered
+//         motionPreset="slideInBottom"
+//       >
+//         <ModalOverlay />
+//         <ModalContent />
+//         <ModalHeader>Title of Modal</ModalHeader>
+//         <ModalCloseButton />
+//         <ModalBody>Lorem.</ModalBody>
+//
+//         <ModalFooter>
+//           <Button colorScheme="skyblue" onClick={onclose()}>
+//             Close
+//           </Button>
+//         </ModalFooter>
+//       </Modal>
+//     </>
+//   );
+// }
+//
+// export default App;
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <Center bg={"tomato"} h={"200px"}>
-        <Square bg={"white"} w={"100px"} h={"100px"}>
-          <ChatIcon />
-          Lorem.
-        </Square>
-      </Center>
+      <Button onClick={onOpen}>모달 열기</Button>
 
-      <Center bg={"yellowgreen.100"} h={"200px"}>
-        <Circle bg={"yellowgreen"} w={"100px"} h={"100px"}>
-          AAA+
-          <PhoneIcon />
-        </Circle>
-      </Center>
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>모달의 제목</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+            aperiam, architecto aspernatur dolorum ex exercitationem, expedita
+            fuga laborum magni molestiae officiis optio porro possimus quae quod
+            vitae voluptatem! Ipsum, officia!
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              닫기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
