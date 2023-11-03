c4646cf72599bd1c5ee889901b23d9870fade702
jeon602 <731aa@naver.com>
2023-11-03 09:35:03 +0900

    children prop 연습 시작 - naver & daum

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
-function App() {
-  return <></>;
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <>
+      <a href={to}>{children}</a>
+      <Button as="a" href={to} colorScheme="skyblue"></Button>
+    </>
+  );
 }
+
+function App() {
+  return (
+    <>
+      <MyLink to="http://www.naver.com">네이버로 </MyLink>
+      <MyLink to="http://www.daum.net">다음으로 </MyLink>
+      {/* a href="http://www.naver.com">네이버로 */}
+      {/* a href=MyLink to="http://www.daumnet">다음으로  */}
+    </>
+  );
+}
+
 export default App;
