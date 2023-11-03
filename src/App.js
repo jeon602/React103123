05a92cf6db3738b5b6ba6a80b7e7981cc0ccd601
jeon602 <731aa@naver.com>
2023-11-03 10:56:01 +0900

    함수명은 handle 이벤트 명으로 짓는 관습이 있음

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
-function MyComp({ children, executeClick }) {
-  return (
-    <div>
-      <Button onClick={executeClick}>{children}</Button>
-    </div>
-  );
-}
-function App(props) {
-  function fun1() {
-    console.log("func1 실행");
-  }
-  let fun2 = () => {
-    console.log("arrow fuction 실행 111");
-  };
 
+//리액트에는 함수 명 작성 관습이 존재하는데
+// handle이벤트명
+//이벤트 명 앞에 handle이라고 작성한다.
+
+function App(props) {
+  function someFuctionName() {
+    console.log("Second");
+  }
+  function handleMouseEnter() {
+    console.log("third");
+  }
+  function handleMouseLeave() {
+    console.log("4th");
+  }
   return (
     <div>
-      <MyComp executeClick={fun1}>Button1 </MyComp>
-      <MyComp executeClick={fun2}>Button2</MyComp>
-      <MyComp
-        executeClick={() => {
-          console.log("arrow fuction 실행 3");
-        }}
-      >
-        Button3
-      </MyComp>
-      <MyComp executeClick={() => console.log("arrow fuction 실행 4")}>
-        Button4
-      </MyComp>
+      <Button onClick={() => console.log("first")}>버튼1</Button>
+      <Button onClick={someFuctionName}>버튼2</Button>
+      <Button onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        버튼3
+      </Button>
     </div>
   );
 }
