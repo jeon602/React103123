0a6bbca097ce0d1b0c63c7337bb9ab7bd9b3f6b2
jeon602 <731aa@naver.com>
2023-11-03 09:09:35 +0900

    Image 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import React from "react";
 import {
   Box,
+  Button,
   Flex,
-  FormControl,
-  FormLabel,
-  Switch,
-  Textarea,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
 } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 function App(props) {
   return (
-    <div>
-      <Box w="480px">
-        <FormControl display={"flex"} alignItems={"center"}>
-          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
-          <Switch size={"lg"} colorScheme="orange" />
-        </FormControl>
-      </Box>
-    </div>
+    <Flex
+      px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
+      justify="space-between"
+      py="8px"
+      my="10px"
+      bg="gray.100"
+    >
+      <Flex gap="7px" align="center">
+        <Text as="b">Navbar</Text>
+        <Button variant={"ghost"}>Home</Button>
+        <Button variant={"ghost"}>Link</Button>
+
+        <Menu>
+          <MenuButton
+            variant="ghost"
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+          >
+            Dropdown
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+
+        <Button variant={"ghost"} isDisabled={true}>
+          Disabled
+        </Button>
+      </Flex>
+      <Flex gap="7px" align="center">
+        <Input size="sm" bg="white" colorScheme="green" />
+        <Button size="sm" variant="outline" colorScheme="green">
+          Search
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
