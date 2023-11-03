24c64884b3acab04c41c86897d4e717aec9102bb
jeon602 <731aa@naver.com>
2023-11-03 12:27:52 +0900

    event.preventDefault(); 기본 기능 취소 : 브라우저의 실행을 막는다.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  let text = "initial text";
-  function handleChange(e) {
-    // console.log("input값이 변경됨");
-    console.log(e.target.value);
-    text = e.target.value; // 실제 택스트는 변경되었으나 리액트에서는 변수를 변경했다고 반영해주지 않는다.
+  function handleClick(e) {
+    e.preventDefault(); //실제 기본으로 해야 하는 일을 막는다. //실행시키지 않으면 클릭했을 때 작동해서 네이버로 이동함.
+    //브라우저가 해야 하는 기본 기능을 취소함---> preventDefault
+    console.log("다른 일을 시킴");
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("다른일을 시킴");
+  }
+
   return (
     <div>
-      <Input onChange={handleChange} bg="tomato" />
-      <Input onChange={handleChange} />
+      <a href="http://www.naver.com" onClick={handleClick}>
+        네이버
+      </a>
+      <div>
+        <form action="http://www.naver.com">
+          <input type="text" name="q" />
+          <button>search</button>
+        </form>
+      </div>
     </div>
   );
 }
