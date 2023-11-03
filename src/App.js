ae7750028e202d8fe6c09320e2cdf36cb6ac8e57
jeon602 <731aa@naver.com>
2023-11-03 16:10:49 +0900

    state를 가공해서 사용할 수 있음.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  function handleInput1Change(e) {
+    setNumber1(Number(e.target.value));
+  }
+  function handleInput2Change(e) {
+    setNumber2(Number(e.target.value));
+  }
+
+  let result = number1 + number2;
+
+  return (
+    <>
+      <div>
+        <input type="number" onChange={handleInput1Change} /> +
+        <input type="number" onChange={handleInput2Change} />= {result}
+      </div>
+    </>
+  );
 }
 export default App;
