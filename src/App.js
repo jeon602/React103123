3b9699aa5faea66b08a203d14a3d644c8c41d754
jeon602 <731aa@naver.com>
2023-11-03 12:36:17 +0900

    event.preventDefault(); 기본 기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 function App() {
-  return <></>;
+  function handleClick(e) {
+    //js로 페이지 이동 : 검색 : location.href
+    //a태그 클릭하면 네이버로 이동할 수 있는 메소드 작성
+    e.preventDefault();
+    window.location.href = "http://www.naver.com";
+  }
+
+  return (
+    <>
+      <a href="http://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
+
 export default App;
