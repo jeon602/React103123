13c35ebb74fb890278623897dd471f5472cece0f
jeon602 <731aa@naver.com>
2023-11-03 11:49:45 +0900

    변수의 값이 변경되어도 리액트의 dom에 반영되지 않는다.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Input } from "@chakra-ui/react";
 
 function App(props) {
+  let text = "initial text";
   function handleChange(e) {
     // console.log("input값이 변경됨");
     console.log(e.target.value);
+    text =e.target.value; // 실제 택스트는 변경되었으나 리액트에서는 변수를 변경했다고 반영해주지 않는다.
   }
   return (
     <div>
