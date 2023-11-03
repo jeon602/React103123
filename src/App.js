cb23ba0bbec48f8d3764728838432602542849a2
jeon602 <731aa@naver.com>
2023-11-03 16:27:11 +0900

    state를 변경해서 input의 value 할당하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import React, { useState } from "react";
-import { Text } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 function App(props) {
   const [number, setNumber] = useState(0);
-  //필요하면 다른 값을 파생시킬 수ㅡ 있다/
-  const isOdd = number % 2 === 1;
-  function handleInputChange(e) {
-    setNumber(Number(e.target.value));
+
+  function handlePlusButtonClick() {
+    setNumber(number + 1);
   }
+
+  function handleMinusButtonClick() {
+    setNumber(number - 1);
+  }
+
   return (
     <div>
-      <input type="number" onChange={handleInputChange} bg="yellowgreen" />
-      <Text>
-        {isOdd && "홀수"}
-        {isOdd || "짝수"}
-      </Text>
+      <input type="number" value={number} />
+      <Box>
+        <Button onClick={handlePlusButtonClick}>+</Button>
+        <Button onClick={handleMinusButtonClick}>-</Button>
+      </Box>
+      <Box>
+        <Button onClick={() => setNumber(number + 1)}>+</Button>
+        <Button onClick={() => setNumber(number - 1)}>-</Button>
+      </Box>
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState } from "react";
+// import { Box, Button } from "@chakra-ui/react";
+//
+// function App(props) {
+//   const [number, setNumber] = useState(0);
+//   function handlePlusButtonClick() {
+//     setNumber(number + 1);
+//   }
+//   function handleMinusButtonClick() {
+//     setNumber(number - 1);
+//   }
+//
+//   return (
+//     <div>
+//       <input type="number" value={number} />
+//       <Box>
+//         <Button onClick={handlePlusButtonClick()}>+</Button>
+//         <Button onClick={handleMinusButtonClick()}>-</Button>
+//       </Box>
+//       <Box>
+//         <button onClick={() => setNumber(number + 1)}>+</button>
+//         <button onClick={() => setNumber(number - 1)}>-</button>
+//       </Box>
+//     </div>
+//   );
+// }
+//
+// export default App;
