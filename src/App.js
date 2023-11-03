e498b9b201891cac54d2a71db93dcfe77eb385d2
jeon602 <731aa@naver.com>
2023-11-03 14:14:28 +0900

    stop propagation은 event bubbling 막는 기능

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from "react";
+import { Center } from "@chakra-ui/react";
 
 function App(props) {
   function handleClick(e) {
-    e.preventDefault(); //실제 기본으로 해야 하는 일을 막는다. //실행시키지 않으면 클릭했을 때 작동해서 네이버로 이동함.
-    //브라우저가 해야 하는 기본 기능을 취소함---> preventDefault
-    console.log("다른 일을 시킴");
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("다른일을 시킴");
+    e.stopPropagation();
+    console.log(e.target.className);
   }
 
   return (
-    <div>
-      <a href="http://www.naver.com" onClick={handleClick}>
-        네이버
-      </a>
-      <div>
-        <form action="http://www.naver.com">
-          <input type="text" name="q" />
-          <button>search</button>
-        </form>
-      </div>
-    </div>
+    <Center className="outerBox" w="200px" h="200px" bg="gold">
+      <Center className="innerBox" w="100px" h="100px" bg="skyblue">
+        <button onClick={handleClick}>button</button>
+      </Center>
+    </Center>
   );
 }
 
