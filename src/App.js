d00f90205ec38a0d0fbe32e7be4e271c18733e33
jeon602 <731aa@naver.com>
2023-11-07 10:58:29 +0900

    일단 커밋

--- src/App.js
+++ src/App.js
@@ -1,4 +1,37 @@
-function App() {
-  return <></>;
+import React, { useState } from "react";
+import * as PropTypes from "prop-types";
+import { Box, Input, Text } from "@chakra-ui/react";
+
+function MyInput(props) {
+  return (
+    <Box>
+      <Input />
+    </Box>
+  );
 }
+
+function MyText(props) {
+  return (
+    <Box>
+      <Text>address</Text>
+      <Button onClick={onclick}>Change Button</Button>
+    </Box>
+  );
+}
+
+function App({ props }) {
+  const [address, setAddress] = useState("");
+
+  function handleInputChange(text) {
+    setAddress(text);
+  }
+
+  return (
+    <div>
+      <MyInput address={address} onChange={handleInputChange} />
+      <MyText address={address} />
+    </div>
+  );
+}
+
 export default App;
